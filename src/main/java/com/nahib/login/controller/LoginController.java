887a2b848d13e6d1ba6c094b003b15b812d78ce4
Nahib <bihan123@126.com>
2020-04-09 21:03:03 +0800

    解决一些bug

--- src/main/java/com/nahib/login/controller/LoginController.java
+++ src/main/java/com/nahib/login/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.nio.charset.StandardCharsets;
 
+
 @RestController
 @RequestMapping("/user")
 public class LoginController {
---
+++ src/main/java/com/nahib/login/repository/UserRepository.java
@@ -0,0 +1,7 @@
+//package com.nahib.login.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository {
+//
+//}
